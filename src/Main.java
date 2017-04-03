091b3248d4725e5a6df383ed6c512c1fffa92ed1
shinabarger <davidshinabarger@gmail.com>
2017-04-03 10:26:58 -0400

    put symbols into arrayList, create symbols.txt file, basic label refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
-    public static String main(String testInput) {
-	// write your code here
-        return "buzz";
+    public static void main(String[] args) {
+        // write your code here
+        addPeriodicTableSymbolsToArrayList();
 
     }
+    private static void addPeriodicTableSymbolsToArrayList() {
+        ArrayList<String> periodicSymbols = new ArrayList<String>();
 
-    //TODO parse data from periodictable.txt and put it into an arrayList 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/Users/davidshinabarger/Desktop/White-Label-FizzBuzz-1/src/Symbols.txt"))) {
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                periodicSymbols.add(currentLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Arrays.toString(periodicSymbols.toArray()));
+    }
+
+    public static String main(String buzz) {
+        return "buzz";
+    }
 }
+
