1b7d389fcc5beb256714306f4a2fb358d4bd89a4
shinabarger <davidshinabarger@gmail.com>
2017-04-10 12:24:45 -0400

    Refactor methods, remove basic test setup, create basic method to turn one or two letter words into the symbol in the periodic table, created a few junit tests, refactored variable names for better readability

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        addPeriodicTableSymbolsToArrayList();
+    public static String main(String inputWord) {
+
+        List<String> periodicSymbols = addTableOfElementsToList();
+
+        String wordOutput = getOutputString(inputWord, periodicSymbols);
+
+        return wordOutput;
 
     }
-    private static void addPeriodicTableSymbolsToArrayList() {
-        ArrayList<String> periodicSymbols = new ArrayList<String>();
+
+    private static List<String> addTableOfElementsToList() {
+        List<String> periodicSymbols = new ArrayList<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/Users/davidshinabarger/Desktop/White-Label-FizzBuzz-1/src/Symbols.txt"))) {
             String currentLine;
@@ -26,10 +32,28 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(Arrays.toString(periodicSymbols.toArray()));
+        return periodicSymbols;
     }
 
-    public static String main(String buzz) {
-        return "buzz";
+    private static String getOutputString(String inputWord, List<String> periodicSymbols) {
+        String wordOutput = null;
+        for (int i = 0; i < 104; i++) {
+            String capitalizedInputWord = inputWord.toUpperCase();
+            String currentPeriodicElement = periodicSymbols.get(i);
+
+            if(capitalizedInputWord.length() <=2) {
+
+                if (periodicSymbols.contains(capitalizedInputWord)) {
+                    wordOutput = capitalizedInputWord;
+                } else if (currentPeriodicElement.equalsIgnoreCase(capitalizedInputWord)) {
+                    wordOutput = currentPeriodicElement;
+                }
+            }
+            //TODO add section if the word input from user is longer than two letters
+
+        }
+        return wordOutput;
+
     }
 }
 
--- src/MainTest.java
+++ src/MainTest.java
@@ -6,12 +6,36 @@ import org.junit.Test;
  */
 
 public class MainTest {
+
     @Test
-    public void main() throws Exception {
-        String actual = Main.main("buzz");
-        String expected = "buzz";
+    public void doesLetterBGetCapitalized() throws Exception {
+        String actual = Main.main("b");
+        String expected = "B";
         Assert.assertEquals(expected, actual);
 
+    }
+
+    @Test
+    public void doesnobecomeNo() throws Exception {
+        String actual = Main.main("no");
+        String expected = "No";
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void doesinbecomeIn() throws Exception {
+        String actual = Main.main("in");
+        String expected = "In";
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    //TODO make this work
+    public void conBecomeCoN() throws Exception {
+        String actual = Main.main("con");
+        String expected = "CoN";
+        Assert.assertEquals(expected, actual);
 
     }
 
